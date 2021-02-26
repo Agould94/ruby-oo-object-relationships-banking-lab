07e7b66b3e49b6fe0066b452c0e859d30b47c0eb
Alexander Gould <alexander.gould94@gmail.com>
2021-02-25 21:08:03 -0800

    commit tree b5ca714a4494fde65a432acf1e4d1535e9b32791

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid?
+      sender.deposit(-amount)
+      receiver.deposit(amount)
+      sender.status = "closed"
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      sender.status = "closed"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if execute_transaction
+      receiver.deposit(-amount)
+      sender.deposit(amount)
+      self.status = "reversed"
+    end
+  end
 end
